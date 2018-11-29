12ba7b71d29540cf28d9da462cb4eec43636809f
nurgeld <nurgeldy@gmail.com>
2018-11-29 19:59:30 +0600

    exclude path to autload.php

--- src/Cli.php
+++ src/Cli.php
@@ -6,6 +6,7 @@ use function \cli\line;
 
 function run()
 {
+    echo 'test run';
     line('Welcome to the Brain Game!');
     $name = \cli\prompt('May I have your name?');
     line("Hello, %s!", $name);
