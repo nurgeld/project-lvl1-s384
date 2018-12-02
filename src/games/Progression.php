7e30ec77d07ab28a3217a3a803132a888500f79b
nurgeld <nurgeldy@gmail.com>
2018-12-02 15:35:13 +0600

    fix bugs in progression game

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -14,12 +14,26 @@ function run()
     $gamePromt = 'What number is missing in the progression?';
 
     $progression = function () {
-        $a1 = rand(1, 15);
-        $d = rand(1, 15);
+        $a1 = rand(1, 10);
+        $d = rand(1, 10);
         $n = 10;
         $randIndex = rand(1, 9);
         $correctAnswer = elementOfProgression($a1, $d, $randIndex);
-        
+        $array = [];
+        $i = 0;
+        while ($i < $n) {
+            if ($i == $randIndex) {
+                $array[] = '..';
+            } else {
+                $array[] = elementOfProgression($a1, $d, $i);
+            }
+            $i += 1;
+        }
+
+        $question = implode(' ', $array);
+        return [$question, $correctAnswer];
+    };
+        /*
         function iter($curr, $a1, $d, $n, $randIndex, $question, $correctAnswer)
         {
             if ($curr = $n) {
@@ -31,7 +45,7 @@ function run()
             return iter($curr + 1, $a1, $d, $n, $randIndex, $question, $correctAnswer);
         }
         return iter(1, $a1, $d, $n, $randIndex, [], $correctAnswer);
-    };
+    };*/
 
     play($gamePromt, $progression);
 }
